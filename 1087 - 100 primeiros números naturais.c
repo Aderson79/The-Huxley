ec41107804b84b358767ce4d9bc51b0029627826
Aderson Jônatas <138100457+Aderson79@users.noreply.github.com>
2023-06-29 04:13:08 -0300

    Create 1087 - 100 primeiros números naturais.c

---
+++ 1087 - 100 primeiros números naturais.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int main(){
+    for(int d=0;d<100;d++){
+        if(d==99){
+            printf("%d",d);
+            break;}
+        printf("%d\n",d);}
+	return 0;}
