bea244b52db0b477f22faf95982cbb28aace4cfd
Aderson Jônatas <138100457+Aderson79@users.noreply.github.com>
2023-06-29 04:45:49 -0300

    .

---
+++ 1099 - Área de um trapézio.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+  int n1,n2,n3;
+  float area,rizz;
+  scanf("%d%d%d",&n1,&n2,&n3);
+  rizz=((n1+n2)*n3);
+  area=rizz/2;
+  printf("%.1f",area); 
+  return 0;}
