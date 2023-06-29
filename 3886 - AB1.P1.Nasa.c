6e21fea4eacd4a9b9f07f42cba333f9ef1a69709
Aderson Jônatas <138100457+Aderson79@users.noreply.github.com>
2023-06-29 15:36:22 -0300

    .

---
+++ 3886 - AB1.P1.Nasa.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    
+    float n,p;
+    scanf("%f",&n);
+    p=n/(9.8);
+    printf("%.2f\n",p*273.42);
+    printf("%.2f\n",p*3.78);
+    printf("%.2f\n",p*8.6);
+    printf("%.2f\n",p*9.8);
+    printf("%.2f\n",p*3.72);
+    printf("%.2f\n",p*24.8);
+    printf("%.2f\n",p*10.5);
+    printf("%.2f\n",p*8.5);
+    printf("%.2f\n",p*10.8);
+    printf("%.2f\n",p*5.88);
+    printf("%.2f",p*1.67);
+	
+	return 0;}
