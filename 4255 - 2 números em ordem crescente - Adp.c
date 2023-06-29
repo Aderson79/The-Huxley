ff5c8e0cc47bbb9938a5786df1427093bb2b497d
Aderson Jônatas <138100457+Aderson79@users.noreply.github.com>
2023-06-29 04:20:40 -0300

    .

---
+++ 4255 - 2 números em ordem crescente - Adp.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main(){
+  int n1,n2;
+  scanf("%d%d",&n1,&n2);
+  if(n1>n2){
+    printf("%d\n",n2);
+    printf("%d",n1);}
+  else{
+    printf("%d\n",n1);
+    printf("%d",n2);}
+  return 0;}
